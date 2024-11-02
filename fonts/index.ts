f72d8d67dc908858026a4649d23c2a7fc4246425
SangSeop Hwang <sshwang12345@gmail.com>
2024-11-02 22:27:42 +0900

    fix: unify Pretendard font

--- fonts/index.ts
+++ fonts/index.ts
@@ -8,43 +8,6 @@ export const breathney = localFont({
 
 export const pretendard = localFont({
   display: 'swap',
+  src: './PretendardVariable.woff2',
   variable: '--font-pretendard',
-  src: [
-    {
-      path: './Pretendard-Black.woff2',
-      weight: '900',
-    },
-    {
-      path: './Pretendard-ExtraBold.woff2',
-      weight: '800',
-    },
-    {
-      path: './Pretendard-Bold.woff2',
-      weight: '700',
-    },
-    {
-      path: './Pretendard-SemiBold.woff2',
-      weight: '600',
-    },
-    {
-      path: './Pretendard-Medium.woff2',
-      weight: '500',
-    },
-    {
-      path: './Pretendard-Regular.woff2',
-      weight: '400',
-    },
-    {
-      path: './Pretendard-Light.woff2',
-      weight: '300',
-    },
-    {
-      path: './Pretendard-ExtraLight.woff2',
-      weight: '200',
-    },
-    {
-      path: './Pretendard-Thin.woff2',
-      weight: '100',
-    },
-  ],
 });
